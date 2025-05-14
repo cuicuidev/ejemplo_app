668c473eea0cc2a7fc0a9717fb461cc2fa42c1c9
Dmitry <dmitry.ryzhenkov@hackaboss.com>
2025-05-14 18:10:02 +0200

    secrets

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import streamlit as st
 import seaborn as sns
 
 def main():
+    DATABASE_URL = st.secrets["DATABASE_URL"]
+
     df = sns.load_dataset("iris")
     st.dataframe(df)
+    st.write(DATABASE_URL)
 
 if __name__ == "__main__":
     main()
